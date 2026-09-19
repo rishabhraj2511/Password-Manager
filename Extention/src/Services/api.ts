3b61d7aba688aa0fe777d9d79f515f0720e90572
Rishabh Raj <rishabhraj8423@gmail.com>
2026-09-19 10:22:17 +0530

    Add Docker deployment setup

--- Extention/src/Services/api.ts
+++ Extention/src/Services/api.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+const apiBaseUrl =
+    import.meta.env.VITE_API_URL ||
+    "http://127.0.0.1:8000";
+
 const api = axios.create({
-    baseURL: "http://127.0.0.1:8000",
+    baseURL: apiBaseUrl,
     headers: {
         "Content-Type": "application/json"
     }
